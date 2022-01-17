889db7631d117e088bcd7b2c58ae5f2ad9e737f3
yuvraj bagale <yuvrajbagale123@email.com>
2022-01-17 22:18:30 +0530

    My Python Progrmas For Practics

---
+++ arithamatics_opretions.py
@@ -0,0 +1,17 @@
+#arithmatic Opreters
+a = 6
+b = 3
+total = a + b
+sub = a - b
+mul = a * b
+div = a / b
+flowdiv = a // b
+mod = a % b
+exponent = a ** b
+print('Addition: Adds one operand to the other: ',a + b)
+print('Subtraction - Subtracts the second operand from the first: ',a - b)
+print('Multiplication *	Multiplies one operand by the other: ',a * b)
+print('Division / Divides the first operand by the second: ',a / b)
+print('Floor division : In Python, // is used to conduct the floor division. It is used to find the floorof the quotient when first operand is divided by the second.',a // b)
+print('Modulo % Divides the first INTEGER operand by the second, and returns the remainder: ',a % b)
+print('Exponentiation ** Lets you refer to a number in terms of a base value and an exponent: ',a ** b)

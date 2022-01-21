a5df5b73fbaece121b05b9701565052b785f02ec
yuvraj bagale <yuvrajbagale123@email.com>
2022-01-21 12:04:17 +0530

    My Python Progrmas For Practics

---
+++ if statement with logical operator.py
@@ -0,0 +1,9 @@
+if 5>2 and 7>3:#condition
+    print("if statement with logical operators")# depening with condition
+    print("statement 2")
+print( 'rest of the code' )#indepening code
+
+if 5>2 or 7<3:#condition
+    print("if statement with logical operators")# depening with condition
+    print("statement 2")
+print( 'rest of the code' )#indepening code

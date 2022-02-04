6b9eb232997f1b7e64db05df7da76a6cabd17fe5
yuvraj bagale <yuvrajbagale123@email.com>
2022-02-04 11:47:51 +0530

    My Python Progrmas For Practics!!!!Keep Practices and Hard Work......

---
+++ String_Functions.py
@@ -0,0 +1,22 @@
+print("+++++++++++++++++#Upper() function#+++++++++++++++++++++")
+name ="yuvraj bagele"
+str1= name.upper()
+print(name)
+print(str1)
+
+print("+++++++++++++++#lower() function#+++++++++++++++")
+name1 = "YUVRAJBAGALE"
+str2 = name1.lower()
+print(name1)
+print(str2)
+
+print("++++++++++++++++++++++Swapcase++++++++++++++++")
+str3=name1.swapcase()
+str4=name.swapcase()
+print(str4)
+print(str3)
+
+print("++++++++++++++++++++++++++ title() +++++++++++++++")
+name2 = "this is most use full fucntion to first letter capital"
+str5 = name2.title()
+print(str5  )

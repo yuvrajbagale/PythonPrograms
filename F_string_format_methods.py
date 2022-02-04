29fc33eb2fb071d5b1e797fdbbd323254c96006a
yuvraj bagale <yuvrajbagale123@email.com>
2022-02-04 11:22:29 +0530

    My Python Progrmas For Practics!!!!Keep Practices and Hard Work......

--- F_string_format_methods.py
+++ F_string_format_methods.py
@@ -51,28 +51,52 @@ print(f"{name:*>8.3s}")
 print(f"{name:^8.3s}")
 print(f"{name:*^8.3s}")
 
-print("+++++++++++++++++++Comma Seprater++++++++++++++++++++++++")
+# Thousand Separator
 price = 1234567890
 print(f"{price:,}")
-print("+++++++++++++++++++++Under Score+++++++++++++++++++++++++++++")
 print(f"{price:_}")
 
-#variable
-name1 = "yuvraj"
-age1 = 27
-print(f"My Name is {name1} and age is {age1}")
+#Variable
+name = "Rahul"
+age= 62
+print(f"My name is {name} and age {age}")
 
-#expreationce
+# Expression
 print(f"{10*8}")
 
+# Expressing a Percentage
 a = 50
 b = 3
-print(f"{a/b:.2f}")
+print(f"{a/b:.2%}")
 
-#tuple
-value = (10,20)
-print(f"{value[0],value[1]}")
+# Accessing arguments� items
+value = (10, 20)
+print(f"{value[0]} {value[1]}")
 
-#dic
-data = {"rahul":200000,"akshay":3000000}
-print(f"{data['rahul']:d} {data['akshay']:d}")
+# Format with Dict
+data = {'rahul': 2000, 'sonam': 3000}
+print(f"{data['rahul']:d} {data['sonam']:d}")
+
+# Calling Function
+name= "yuvraj"
+print(f"{name}")
+print(f"{name.upper()}")
+
+# Using object created from class
+#print(f"{obj.name}")
+
+# Curly Braces
+print(f"{10}")
+print(f"{{10}}")
+
+# Date and Time
+from datetime import datetime
+today = datetime(2019, 10, 5)
+print(f"{today}")
+print(f"{today:%d-%b-%Y}")
+print(f"{today:%d/%b/%Y}")
+print(f"{today:%b/%d/%Y}")
+print(f"{today:%b %d, %Y}")
+
+# Datetime Directive https://docs.python.org/3.7/library/datetime.html#strftime-and-strptime-behavior
+

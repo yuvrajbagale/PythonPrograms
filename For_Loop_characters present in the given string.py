9e10d82db307c2b1c0c1703f5cc133b485d2b9f0
yuvraj bagale <yuvrajbagale123@email.com>
2022-02-16 14:51:15 +0530

    keep practicesing

---
+++ For_Loop_characters present in the given string.py
@@ -0,0 +1,5 @@
+#to print characters present in the given string
+a = "akshay bagale"
+for x in a:
+    print(x)
+    
---
+++ read employee data.py
@@ -0,0 +1,13 @@
+#write a program to read employeed data from the keybord and print that data
+
+eno=int(input("Enter Employee No:"))
+ename=input("Enter Employee name:")
+esal=float(input("Enter Employee salary:"))
+eaddr=input("Enter Employee address:")
+married=bool(input("Employee married?[True|False]:"))
+print("please confirm information")
+print("Employee no:",eno)
+print("Employee name:",ename)
+print("Employee sal:",esal)
+print("Employee address:",eaddr)
+print("Employee married?:",married)

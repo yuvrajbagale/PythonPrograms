3a3d803bd15bedffb0bf45b628193224cf7b7c4a
yuvraj bagale <yuvrajbagale123@email.com>
2022-04-03 16:40:54 +0530

    keep practiceing and grow up your knowleged

---
+++ While_Loop_statmanet.py
@@ -0,0 +1,10 @@
+#while loop - if we wants to execute a gruop of statment itractivly unit some condition false, we should go for while loop
+# syntax - 
+# while condition:
+#       body
+
+#Ex.1 To print numbers from 1 to 10 by using while loop
+x = 1
+while x<=10:
+    print(x)
+    x=x+1

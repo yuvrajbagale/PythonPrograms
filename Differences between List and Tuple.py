884bf7cfd845416494b53a7b53a97ca5d60eac4d
yuvraj bagale <yuvrajbagale123@email.com>
2022-04-05 14:28:09 +0530

    Tuple Functions

---
+++ Differences between List and Tuple.py
@@ -0,0 +1,19 @@
+# Differnaces Between list and Tuple:
+# list and tuple are already same except small difference :  list object are mutable where as tuple object are immutable.
+#in both case insertion order is preserved, duplicates ojects are allowed, hetrogeneus objects are allowed, index and slicing are supporeded.
+
+# list - list is a comma seprated values within square brackets and Square brackets are mandatory
+
+# Tuple - tuple is a comma separated value within paranethesis and paranethesis are optional
+
+# List object are mutable i.e once we create list object we can perform any chenges in that objecct
+
+# Tuple object are immutable i.e once we create tuple object we cannot changes its content. tuple object does not suppoert item assinement
+
+# list if the content is not fixed and keep on changing then we should go for list.
+
+# Tuple if the content is fixed and never changes then we should go for tuple
+
+#list objects can not be used as keys for dictionary because keys should be hashable and immutable
+
+#Tuple objects can be used as keys for dictionary because key should be hashable and immutable.

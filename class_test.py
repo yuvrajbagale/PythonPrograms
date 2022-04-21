4c933e17cf835fa5d6b625ed7fe3ca0846593403
Yuvraj Bagale <58872409+yuvrajbagale@users.noreply.github.com>
2022-04-21 12:20:48 +0530

    oops

---
+++ class_test.py
@@ -0,0 +1,11 @@
+class Test:
+    def __init__(self):
+        self.name='yuvraj'
+        self.addr='Pune'
+    
+    def display_text(self):
+        print('my name is:',self.name)
+        print('my address is',self.addr)
+
+t=Test()
+t.display_text()
